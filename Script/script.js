fbcfdd20d2d98dea692434f8863ae5feeb1c65e4
Herick <herickmartins86@gmail.com>
2024-12-09 11:19:03 -0300

    Informações

--- Script/script.js
+++ Script/script.js
@@ -77,3 +77,22 @@ document.addEventListener("DOMContentLoaded", function () {
     // Verifica logo no carregamento
     handleScroll();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach((item) => {
+        const question = item.querySelector(".faq-question");
+
+        question.addEventListener("click", () => {
+            // Fecha todas as outras FAQs
+            faqItems.forEach((otherItem) => {
+                if (otherItem !== item) {
+                    otherItem.classList.remove("active");
+                }
+            });
+            // Abre/Fecha a FAQ clicada
+            item.classList.toggle("active");
+        });
+    });
+});
